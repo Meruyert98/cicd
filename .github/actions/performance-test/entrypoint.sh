e399f3d09f2726972a43fa37835e08d12216571e
Meruyert98 <11d.meruyerts@gmail.com>
2025-05-19 12:31:46 -0400

    1

--- .github/actions/performance-test/entrypoint.sh
+++ .github/actions/performance-test/entrypoint.sh
@@ -7,7 +7,7 @@ CONTAINER_NAME=$1
 URL=$2
 
 echo "Starting Docker container for performance testing..."
-docker run -d --name "$CONTAINER_NAME" -p 8080:8080 my-python-app:"${GITHUB_SHA}"
+docker run -d --name "$CONTAINER_NAME" -p 8080:8080 flask-app:"${GITHUB_SHA}"
 
 echo "Running JMeter tests..."
 # Download JMeter if not already downloaded
